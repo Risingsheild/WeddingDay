4ddab3bd042c560cb7a90689ea5e7e9e6b4f49b6
Wells <NickWells@reddotcorp.com>
2025-05-05 17:02:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Gallery } from './pages/Gallery';
 
 function App() {
   return (
